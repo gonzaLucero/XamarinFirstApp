5334428e34edb7dfa3ecf634b85e18677298e29c
Gonzalo Lucero <glucero6@hexacta.com>
2020-11-20 12:03:29 -0300

    Primer commit

---
+++ DemoCorvel/DemoCorvel/DemoCorvel/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoCorvel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            myLabel.Text = string.Format("Presioné {0} veces", count);
+            this.DisplayAlert("Hola", "Mundo!", "Aceptar");
+        }
+    }
+}
